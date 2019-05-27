e67158279dde525306b68d4a08cf1223f860f384
Mitch Lloyd <mitch.lloyd@gmail.com>
2019-05-27 09:20:00 -0700

    Remove protoc-gen-twirp_typescript test

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,13 +2,12 @@ import * as pb from './service.pb';
 import Example = pb.twitch.twirp.example;
 import { AsyncServer } from './async-server';
 import { createHaberdasherHandler } from './service';
-import { createHaberdasher } from './service.twirp';
 import request from 'request-promise-native';
 import { createTwirpClient } from './client';
 import * as protobuf from 'protobufjs';
 import * as path from 'path';
 
-test('Handling a Twirp protobuf call with protoc-gen-twirp_typescript', async () => {
+test('Handling a Twirp protobuf call', async () => {
   function makeHat(size: Example.Size): Promise<Example.Hat> {
     const response = new Example.Hat({
       color: 'red',
@@ -18,39 +17,7 @@ test('Handling a Twirp protobuf call with protoc-gen-twirp_typescript', async ()
 
     return Promise.resolve(response);
   }
-  
-  const handler = createHaberdasherHandler({
-    makeHat,
-  });
 
-  const s = await new AsyncServer(handler).listen();
-
-  const client = createHaberdasher('http://localhost:8000');
-
-  const response = await client.makeHat({
-    inches: 42,
-  });
-
-  try {
-    expect(response.size).toEqual(42);
-    expect(response.name).toEqual('fancy hat');
-    expect(response.color).toEqual('red');
-  } finally {
-    await s.close();
-  }
-});
-
-test('Handling a Twirp protobuf call with custom ts-twirp-server client', async () => {
-  function makeHat(size: Example.Size): Promise<Example.Hat> {
-    const response = new Example.Hat({
-      color: 'red',
-      name: 'fancy hat',
-      size: size.inches,
-    });
-
-    return Promise.resolve(response);
-  }
-  
   const handler = createHaberdasherHandler({
     makeHat,
   });
@@ -88,42 +55,7 @@ test('Handling a Twirp JSON call', async () => {
 
     return Promise.resolve(response);
   }
-  
-  const handler = createHaberdasherHandler({
-    makeHat,
-  });
 
-  const s = await new AsyncServer(handler).listen();
-  const responseJSON = await request(`http://localhost:8000/twitch.twirp.example.Haberdasher/MakeHat`, {
-    body: JSON.stringify({
-      inches: 42,
-    }),
-    headers: {
-      "Content-Type": "application/json",
-    }
-  });
-  const response = JSON.parse(responseJSON);
-
-  try {
-    expect(response.size).toEqual(42);
-    expect(response.name).toEqual('fancy hat');
-    expect(response.color).toEqual('red');
-  } finally {
-    await s.close();
-  }
-});
-
-test('Returning the Content-Length header', async () => {
-  function makeHat(size: Example.Size): Promise<Example.Hat> {
-    const response = new Example.Hat({
-      color: 'red',
-      name: 'fancy hat',
-      size: size.inches,
-    });
-
-    return Promise.resolve(response);
-  }
-  
   const handler = createHaberdasherHandler({
     makeHat,
   });
@@ -141,6 +73,10 @@ test('Returning the Content-Length header', async () => {
 
   try {
     expect(response.headers['content-length']).toEqual("44");
+    const json = JSON.parse(response.body);
+    expect(json.size).toEqual(42);
+    expect(json.name).toEqual('fancy hat');
+    expect(json.color).toEqual('red');
   } finally {
     await s.close();
   }
